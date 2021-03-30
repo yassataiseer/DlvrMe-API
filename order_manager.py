525ed546de20aae13d91422dba84258fc73ff18d
Yassa Taiseer <yassataiseer@gmail.com>
2021-03-29 20:09:35 -0400

    commit on main

--- order_manager.py
+++ order_manager.py
@@ -3,24 +3,28 @@ import os
 import requests, json
 
 
-db =mysql.connector.connect(
-    host = "localhost",
-    user = "root",
-    passwd = "new_password",
-    database = "dlvrme"
-)
-
 
 class order:
 
     def delete_order(Username,Address,Item,Price,User_Info):
+        db =mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        passwd = "new_password",
+        database = "dlvrme")
         mycursor = db.cursor()
         Price1 = float(Price)
         mycursor.execute("DELETE FROM deliveries WHERE Username = %s AND Address = %s AND Item = %s AND Price = %s AND User_Info = %s",(Username,Address,Item,Price1,User_Info))
         db.commit()
         mycursor.close()
+        db.close()
         return {"Status":True}
     def add_order(Username,Address,Item,Price,User_Info):
+        db =mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        passwd = "new_password",
+        database = "dlvrme")
         mycursor = db.cursor()
         try:
             url = 'http://photon.komoot.de/api/?q='
@@ -34,12 +38,19 @@ class order:
             mycursor.execute("INSERT INTO deliveries (Username,Address,Latitude,longitude,Item,Price,User_Info) VALUES (%s,%s,%s,%s,%s,%s,%s)",(Username,Address,lat,lon,Item,Price,User_Info))
             db.commit()
             mycursor.close()
+            db.close()
             return {"Status":True}
         except IndexError:
+            db.close()
             return {"Status":False}
     def edit_order(Username,Address,Item,Price,User_Info):
         pass
     def get_order():
+        db =mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        passwd = "new_password",
+        database = "dlvrme")
         mycursor = db.cursor()
         data = ["Name","Address","Latitude","Longitude","Item","Price","Description"]
         mycursor.execute('SELECT * FROM deliveries')
@@ -49,8 +60,14 @@ class order:
             i = dict(zip(data,i))
             data1.append(i)
         mycursor.close()
+        db.close()
         return data1
     def get_order_specific_person(username):
+        db =mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        passwd = "new_password",
+        database = "dlvrme")
         mycursor = db.cursor()
         data = ["Name","Address","Latitude","Longitude","Item","Price","Description"]
         mycursor.execute("SELECT * FROM deliveries WHERE Username = %s",(username,))
@@ -60,12 +77,19 @@ class order:
             data2 = dict(zip(data,i))
             final.append(data2)
         mycursor.close()
+        db.close()
         return final
     def grab_address(Address):
+        db =mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        passwd = "new_password",
+        database = "dlvrme")
         mycursor = db.cursor()
         mycursor.execute("SELECT * FROM deliveries WHERE Address = %s",(Address,))
         data = mycursor.fetchall()
         mycursor.close()
+        db.close()
         if len(data)==0:
             return  {"Status":False}
         else:
