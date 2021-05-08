12809473eacc6dac6c1df9af33bea732570503fb
Yassa Taiseer <yassataiseer@gmail.com>
2021-05-08 16:11:55 -0400

    Orders auth is done

--- Orders/order_template.py
+++ Orders/order_template.py
@@ -1,32 +1,50 @@
 from flask import Flask, jsonify,Blueprint
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from Orders.order_manager import order
+from flask_httpauth import HTTPBasicAuth
+
+
+
 order_template = Blueprint("order_template",__name__)
 
 api = Api(order_template)
-
+auth = HTTPBasicAuth()
+USER_DATA = {
+    "Yassa Taiseer": "yassa123"
+}
+@auth.verify_password
+def verify(username, password):
+    if not (username and password):
+        return False
+    return USER_DATA.get(username) == password
 """Orders"""
 class mk_order(Resource):
+    @auth.login_required
     def get(self,Username,Address,Item,Price,User_Info):
         status = order.add_order(Username,Address,Item,Price,User_Info)
         return jsonify(status)
 class del_order(Resource):
+    @auth.login_required
     def get(self,Username,Address,Item,Price,User_Info):
         status = order.delete_order(Username,Address,Item,Price,User_Info)
         return jsonify(status)
 class all_order(Resource):
+    @auth.login_required
     def get(self):
         data = order.get_order()
         return jsonify(data)
 class spec_order(Resource):
+    @auth.login_required
     def get(self,Username):
         data = order.get_order_specific_person(Username)
         return jsonify(data)
 class find_address(Resource):
+    @auth.login_required
     def get(self,Address):
         status = order.grab_address(Address)
         return jsonify(status)
 class validate_address(Resource):
+    @auth.login_required
     def get(self,Address):
         status = order.validate_address(Address)
         return jsonify(status)
