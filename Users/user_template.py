795fe5d51e810584a6282a43804e38ff5628168f
Yassa Taiseer <yassataiseer@gmail.com>
2021-05-08 16:10:09 -0400

    auth for Users db added

--- Users/user_template.py
+++ Users/user_template.py
@@ -1,29 +1,41 @@
 from flask import Flask, jsonify,Blueprint
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from Users.user_manager import user
+from flask_httpauth import HTTPBasicAuth
 user_template = Blueprint("user_template",__name__)
 
 api = Api(user_template)
+auth = HTTPBasicAuth()
+
+
+USER_DATA = {
+    "Yassa Taiseer": "yassa123"
+}
+@auth.verify_password
+def verify(username, password):
+    if not (username and password):
+        return False
+    return USER_DATA.get(username) == password
 
 """Users"""
 class mk_user(Resource):
+    @auth.login_required
     def get(self,Username,Password):
         boolean = user(Username,Password)
         status = boolean.add_user()
         return jsonify(status)
-
 class validate_user(Resource):
+    @auth.login_required
     def get(self,Username,Password):
         boolean = user(Username,Password)
         status = boolean.check_user()
         return jsonify(status)
-
 class delete_user(Resource):
+    @auth.login_required
     def get(self,Username,Password):
         boolean = user(Username,Password)
         status = boolean.delete_user()
         return jsonify(status)
-
 
 
 api.add_resource(mk_user,"/mk_user/<string:Username>/<string:Password>")
