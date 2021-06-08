d702e930d3220b1ce15ddbfacc577c85553bb7cb
Yassa Taiseer <yassataiseer@gmail.com>
2021-06-08 00:43:19 -0400

    test workss

---
+++ tests/test_app.py
@@ -0,0 +1,10 @@
+
+import json
+
+
+def test_index(app, client):
+    del app
+    res = client.get('/')
+    assert res.status_code == 404
+    #expected = {'hello': 'world'}
+    #assert expected == json.loads(res.get_data(as_text=True))
