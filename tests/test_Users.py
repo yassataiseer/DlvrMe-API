5dfab6ef8d592c7059dacf90a01d87db447def89
Yassa Taiseer <yassataiseer@gmail.com>
2021-06-24 01:54:35 -0400

    added comments to testing manual

--- tests/test_Users.py
+++ tests/test_Users.py
@@ -3,12 +3,16 @@ import json
 
 from requests.auth import HTTPBasicAuth
 import base64
+## Check for basic connection working
+## Server is meant to show 404(see app.py)
 def test_index(app, client):
     del app
     res = client.get('/')
     assert res.status_code == 404
 
-
+## Will insert data into users columns
+## Then delete it from the same column
+## Check to see if there are successfull status codes(200)
 def test_delete(app, client):
     del app
     valid_credentials = base64.b64encode(b"Yassa Taiseer:yassa123").decode("utf-8")
