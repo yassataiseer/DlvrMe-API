d24311d4964b7e9543adcc888cc546005bde1022
Yassa Taiseer <yassataiseer@gmail.com>
2021-06-24 02:10:15 -0400

    finished comments for app.py

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ import mysql.connector
 from flask import Flask, jsonify
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 import os
-#from user_manager import user
-#from order_manager import order
 from Users.user_template import user_template
 from Orders.order_template import order_template
+##imports of packages made for users and deliveries tables
 from models import User,db
 from decouple import config
 from flask_sqlalchemy import SQLAlchemy
@@ -14,11 +13,13 @@ host = config('HOST')
 user = config('USERNAME') 
 passwd = config('PASSWORD')
 database = config('DATABASE')
+#load env var
 app = Flask(__name__)
 api = Api(app)
 app.register_blueprint(user_template,url_prefix="/Users")
 app.register_blueprint(order_template,url_prefix="/Orders")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://'+user+':'+passwd+'@'+host+'/'+database
+##connect to mysql via SQLALCHEMY
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 #app.config['SQLALCHEMY_POOL_RECYCLE'] = 299
 #app.config['SQLALCHEMY_POOL_TIMEOUT'] = 20
@@ -34,6 +35,7 @@ db.init_app(app)
 @app.errorhandler(404)
 def resource_not_found(e):
     return jsonify(error=str(e)), 404
+## return 404 for when app is run on base link('/')
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0',debug=True)
