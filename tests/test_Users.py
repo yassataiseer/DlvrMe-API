757366cbf3692226121ada6a5649e59a257e76d8
Yassa Taiseer <yassataiseer@gmail.com>
2021-06-24 01:48:10 -0400

    change tests

--- tests/test_Users.py
+++ tests/test_Users.py
@@ -12,8 +12,8 @@ def test_index(app, client):
 def test_delete(app, client):
     del app
     valid_credentials = base64.b64encode(b"Yassa Taiseer:yassa123").decode("utf-8")
+    res = client.get('/Users/mk_user/Fake_name/fake_password',headers={"Authorization": "Basic " + valid_credentials})
     res = client.get('/Users/delete_user/Fake_name/fake_password',headers={"Authorization": "Basic " + valid_credentials})
-    print(res)
     assert res.status_code == 200
     expected = { "Status": True }
     assert expected == json.loads(res.get_data(as_text=True))
